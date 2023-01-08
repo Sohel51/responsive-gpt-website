3aba116d9b3d8d0b9a640257ca0c29bd4947b44b
Sohel51 <sohelcutyy@gmail.com>
2023-01-08 23:45:58 +0600

    Complete Single Page Frontend Website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Brand from './components/brand/Brand';
+import CTA from './components/cta/CTA';
+import Navbar from './components/navbar/Navbar';
+import Blog from './containers/blog/Blog';
+import Features from './containers/features/Features';
+import Footer from './containers/footer/Footer';
+import Header from './containers/header/Header';
+import Possibility from './containers/possibility/Possibility';
+import WhatGPT3 from './containers/whatGPT3/WhatGPT3';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
